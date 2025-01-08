2f3a6d173cacb0b91055782bcb89a1699e00882b
ZyChua2000 <c.zhengyang@digipen.edu>
2025-01-08 21:03:15 +0800

    Initial commit

---
+++ core/src/main/kotlin/com/csd3156/group6/Main.kt
@@ -0,0 +1,6 @@
+package com.csd3156.group6
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Main : ApplicationAdapter()
